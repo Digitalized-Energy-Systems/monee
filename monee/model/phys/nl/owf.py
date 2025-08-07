ce56e52769f5a47ab1c540638f684acbd1c796c4
Rico Schrage <rico.schrage@uni-oldenburg>
2025-08-07 16:52:59 +0200

    Pre-commit

--- monee/model/phys/nl/owf.py
+++ monee/model/phys/nl/owf.py
@@ -1,8 +1,10 @@
 from . import hydraulics
 
+
 # laminar flow Dary friction
 def darcy_friction(reynolds_var):
     return 64 / (reynolds_var + 1)
+
 
 # https://apps.dtic.mil/sti/citations/AD0874542
 def darcy_weisbach_equation(
@@ -20,18 +22,15 @@ def darcy_weisbach_equation(
     """
     Original
     return (p_start_var - p_end_var) * on_off ==(
-        swamee_jain(reynolds_var, diameter_m, roughness, kwargs["log_impl"]) * 
-        (pipe_length / diameter_m) * 
+        swamee_jain(reynolds_var, diameter_m, roughness, kwargs["log_impl"]) *
+        (pipe_length / diameter_m) *
         (fluid_density * -velocity_var*abs(velocity_var) / 2)
     )
     """
     # reformulated to optimize numerical stability
-    # note swamee jain decreases stability due to the log
-    return -velocity_var*abs(velocity_var) * hydraulics.swamee_jain(reynolds_var, diameter_m, roughness, kwargs["log_impl"]) == on_off * (
-        2 * (p_start_var - p_end_var) /
-        (
-            (pipe_length / diameter_m) *
-            fluid_density
-        )
+    # note swamee-jain decreases stability due to the log
+    return -velocity_var * abs(velocity_var) * hydraulics.swamee_jain(
+        reynolds_var, diameter_m, roughness, kwargs["log_impl"]
+    ) == on_off * (
+        2 * (p_start_var - p_end_var) / ((pipe_length / diameter_m) * fluid_density)
     )
-
