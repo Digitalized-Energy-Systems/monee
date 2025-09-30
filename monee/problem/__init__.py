c6bb08483ffa91a871c89c95822c596ddfae001c
Rico Schrage <rico.schrage@uni-oldenburg.de>
2025-09-30 02:08:06 +0200

    Fixing doctest optimization problem.

--- monee/problem/__init__.py
+++ monee/problem/__init__.py
@@ -1,12 +1,15 @@
 import monee.model as md
-from .core import OptimizationProblem
-from .core import Constraints
-from .core import Objectives
 from .load_shedding import (
     create_load_shedding_optimization_problem,
     create_ls_init_optimization_problem,
 )
 from .metric import GeneralResiliencePerformanceMetric
+from monee.problem.core import (
+    AttributeParameter,
+    Constraints,
+    Objectives,
+    OptimizationProblem,
+)
 
 
 def calc_general_resilience_performance(network: md.Network, **kwargs):
